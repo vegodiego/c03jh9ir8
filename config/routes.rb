19862854e11d083a80d9624aac9490c536a83678
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-11-28 12:09:53 -0500

    mejorando crear actor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "actors", to: "actors#index"
-  get "actors/new", to: "actors#form"
+  get "actors/new", to: "actors#new"
   post "actors", to: "actors#create"
 end
