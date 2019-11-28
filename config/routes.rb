636752c522c6aa3a3c37822ad0cba8b95b5b425d
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-11-28 11:52:06 -0500

    mejorando la funcionalidad crear actores

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "actors", to: "actors#index"
-  get "form", to: "actors#form"
-  post "actors", to: "actors#envio"
+  get "actors/new", to: "actors#form"
+  post "actors", to: "actors#create"
 end
