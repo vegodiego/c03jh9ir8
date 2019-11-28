69014cb2e908653df310883f4d7b00d04b47ac23
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-11-28 11:31:27 -0500

    funcionalidad crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,20 @@ class ActorsController < ApplicationController
     def index
         @actors=Actor.all
     end
+
+    def form
+        @actor=Actor.new
+    end
+
+    def actor_params
+        params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :death_date, :death_place)
+    end
+
+    def envio
+        @actor=Actor.new(actor_params)
+        if @actor.save
+            redirect_to actors_path
+        end        
+    end
+
 end
